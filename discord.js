fc0c037ddac52f57babfbf7bd254b241ee91cc8d
zIvan-2001 <ivan.antonio@tecsup.edu.pe>
2023-08-10 17:06:14 -0500

    v1.1

--- discord.js
+++ discord.js
@@ -1,4 +1,7 @@
 const { Client, IntentsBitField } = require("discord.js");
+const User = require("./src/model/whiteList.model.js");
+require("dotenv").config();
+
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
@@ -8,6 +11,37 @@ const client = new Client({
   ],
 });
 
+const ConnectDiscord = () => {
+  try {
+    client.login(`${process.env.DISCORD_KEY}`);
+    client.on("ready", () => {
+      console.log(`Logged in as ${client.user.tag}!`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const ListRoles = () => {
+  client.on("messageCreate", async (message) => {
+    if (message.content === "oplist") {
+      const usersId = await User.find();
+      usersId.forEach(async (user) => {
+        const member = await message.guild.members.fetch(user.idUser);
+        if (member) {
+          // Obtener los roles del usuario
+          const roles = member.roles.cache.map((role) => role.name);
+          const rolesString = roles.join(", ");
+          message.channel.send(
+            `Roles de ${member.user.username}: ${rolesString}`
+          );
+        }
+      });
+    }
+  });
+};
+
 module.exports = {
-  client,
+  ConnectDiscord,
+  ListRoles,
 };
